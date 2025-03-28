b773d51d13d4d52e16c1afd4ea8a1aa33d72bd50
MohdThaha <165124426+MohdThaha@users.noreply.github.com>
2025-03-28 18:22:41 +0530

    new

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ server.use(customerRouter)
 const PORT = 4002
 
 server.get('/',(req,res)=>{
-    res.status(200).json("Dev Sewnex server started - Customer v1")
+    res.status(200).json("SBX Sewnex server started - Customer v1.0")
 })
 
 const app = server.listen(PORT, () => {
